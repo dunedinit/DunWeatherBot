3a31a782f20156aa2f4eafe8deaacc4182727049
Jamie Clague <jamieclague@gmail.com>
2017-01-11 23:25:36 +0000

    Changing the bot - MILESTONE

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -3,6 +3,7 @@
 const getCurrentWeather = require('./lib/getCurrentWeather')
 
 exports.handle = function handle(client) {
+
   const sayHello = client.createStep({
     satisfied() {
       return Boolean(client.getConversationState().helloSent)
@@ -53,6 +54,27 @@ exports.handle = function handle(client) {
     },
   })
 
+  const collectName = client.createStep({
+    satisfied() {
+      return Boolean(client.getName().Name)
+    },
+
+    extractInfo() {
+     const city = client.getFirstEntityWithRole(client.getMessagePart(), 'name')
+      if (name) {
+        client.updateName({
+          weatherCity: name,
+        })
+        console.log('User wants the weather in:', city.value)
+      }
+    },
+
+    prompt() {
+      client.addResponse('get_answer/name')
+      client.done()
+    },
+  })
+
   const provideWeather = client.createStep({
     satisfied() {
       return false
@@ -90,9 +112,10 @@ exports.handle = function handle(client) {
   client.runFlow({
     classifications: {},
     streams: {
-      main: 'getWeather',
+      main: 'collectName',
       hi: [sayHello],
-      getWeather: [collectCity, provideWeather],
+      
+      //getWeather: [collectCity, provideWeather],
     }
   })
 }
